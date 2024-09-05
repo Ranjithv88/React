35c011e4eddcc2d7d9927cc4174fa2054a07ea48
ranjithv88 <ranjithkumar22445588@gmail.com>
2024-09-05 12:38:39 +0530

    complete

--- App/src/components/content/contentPage.js
+++ App/src/components/content/contentPage.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './contentPage.css';
+import two from './images/2.png';
+import code from './images/code.png';
+import bg from './images/bg.png';
+import bg2 from './images/bg2.png';
+import rock1 from './images/rock1.png';
+import rock2 from './images/rock2.png';
+import rock3 from './images/rock3.png';
 
 function ContentOfWeb() {
     return (
         <div className="content">
-            {/*======== Icon Bootstrap Link ========*/}
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"/>
-            {/*======== Footer On WebSites ========*/}
-            <div className="Outer">
-                
+            
+            {/* Body Of The Website */}
+            <div className="banner">
+                <div className="product">
+                    <div className="soda" style={{ '--url': `url(${bg})` }}></div>
+                    <div className="soda" style={{ '--url':  `url(${bg2})` }}></div>
+                </div>
+                <div className="rock">
+                    <img src={rock1} alt="Rock 1"/>
+                    <img src={rock2} alt="Rock 2"/>
+                    <img src={rock3} alt="Rock 3"/>
+                </div>
             </div>
+
         </div>
     );
 }
 
 export default ContentOfWeb;
+
 
